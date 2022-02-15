d32007391e0d05494731cd72f05bcafee7c8b405
DenyX5518 <96446883+DenyX5518@users.noreply.github.com>
2022-02-16 00:53:00 +0500

    new

---
+++ JS.js
@@ -0,0 +1,11 @@
+$(document).ready(function($){
+    $(".bodding").click(function (){
+       window.location.href = "A:/Web/About.html"
+    });
+    $(".bodding_2").click(function (){
+        window.location.href = "A:/Web/Contact-Data.html"
+    });
+    $(".bodding_3").click(function (){
+        window.location.href = "A:/Web/Price-list.html"
+    });
+});
